147ecd8e834c225f640eccd37530a1df970f1146
Abhishek Behera <lutun2025@gmail.com>
2023-10-06 00:25:42 +0530

    Add files via upload

---
+++ JavaScript_pw/Assignment/Counter App/script.js
@@ -0,0 +1,32 @@
+// Getting the html elements
+
+const decreamentBtn = document.getElementById("decreamentBtn")
+const increamentBtn = document.getElementById("increamentBtn")
+const resetBtn = document.getElementById("resetBtn");
+const displayValue = document.getElementById("displayValue");
+
+// for decreament button click
+
+decreamentBtn.addEventListener("click" , () => {
+    const value = Number(displayValue.innerText)
+    if(value > 0) {
+        displayValue.innerText = value - 1;
+    } else {
+        alert("Negative value not allowed")
+    }
+})
+
+// for increment button click
+increamentBtn.addEventListener("click", () => {
+    const value = Number(displayValue.innerText);
+    if (value >= 10) {
+      alert("10+ values are not allowed");
+    } else {
+      displayValue.innerText = value + 1;
+    }
+  });
+  
+  // for reset button click
+  resetBtn.addEventListener("click", () => {
+    displayValue.innerText = 0;
+  });
